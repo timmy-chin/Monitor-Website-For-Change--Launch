17ea28cba847214f321b2c2389f3320b78520a9b
Timmy Chin <82336477+timmy-chin@users.noreply.github.com>
2021-04-13 00:27:43 -0700

    Update Santa Clara Vaccine Hunter.py

--- Santa Clara Vaccine Hunter.py
+++ Santa Clara Vaccine Hunter.py
@@ -12,41 +12,41 @@ import requests
 import time
 from datetime import datetime
 
-#Paste your chromedriver location down here
-Chromedriverlocation = 'Paste here'
+# Paste your chromedriver location down here
+Chromedriverlocation = input("Chromedriver file location:")
 
-#Which website do you want to monitor
-website = 
+# Which website do you want to monitor
+website = input("Website URL:")
 
-#How often do you want to check the website for change? Type it in seconds (ie. 5 minutes = 300)
-checkingtime = 
+# How often do you want to check the website for change? Type it in seconds (ie. 5 minutes = 300)
+checkingtime = input("Check for Update every (in seconds):")
 
-#For how long do you want this program to be monitoring the website? Type it in hours (ie. 1 hour = 1)
-monitoringtime = 
+# For how long do you want this program to be monitoring the website? Type it in hours (ie. 1 hour = 1)
+monitoringtime = input("Run this program for (in hours):")
 
-workingtime = 3600*monitoringtime/checkingtime
+tagname = input('tagname that surrounds word or phrase:')
 
-#calculate working time and insert it below if error occurs
-for number in range(workingtime):
-    #This part is tricky, paste these 4 lines of code into a separate python file to find your changing word
+workingtime = 3600 * int(monitoringtime) / int(checkingtime)
+
+# calculate working time and insert it below if error occurs
+for number in range(int(workingtime)):
+    # This part is tricky, paste these 4 lines of code into a separate python file to find your changing word
     source = requests.get(website).text
     soup = BeautifulSoup(source, 'lxml')
-    #Find out what are the tags that surround the word you want to monitor (ie. if the tag for your word is div, type div below)
-    tagname = 
+    # Find out what are the tags that surround the word you want to monitor (ie. if the tag for your word is div, type div below)
     word = soup.find(tagname)
-    #Find out what are the more specific tags surrounding the word you want to monitor (ie. if you see h3 and span, type word.h3.span.text)
+    # Find out what are the more specific tags surrounding the word you want to monitor (ie. if you see h3 and span, type word.h3.span.text)
     headline = word.text
-    #Use print(headline) to check if you got the right word. If not, keep trying to get the word by changing tag names that surrounds the word until you get the right word printed
-    #Once you got the right word, copy paste all 4 lines back here
+    # Use print(headline) to check if you got the right word. If not, keep trying to get the word by changing tag names that surrounds the word until you get the right word printed
+    # Once you got the right word, copy paste all 4 lines back here
 
-    time.sleep(checkingtime)
-    
-    #Copy paste the 4 lines of code here too, but change headline to newheadline, you should be able to run it by now.
+    time.sleep(int(checkingtime))
+
+    # Copy paste the 4 lines of code here too, but change headline to newheadline, you should be able to run it by now.
     source = requests.get(website).text
     soup = BeautifulSoup(source, 'lxml')
     word = soup.find(tagname)
     newheadline = word.text
-
 
     if headline == newheadline:
         now = datetime.now()
@@ -61,6 +61,7 @@ for number in range(workingtime):
         browser = webdriver.Chrome(Chromedriverlocation)
         browser.get(website)
         break
+
 
 
 
